23dfc639c1bed4879a6f5c936407b846db47d86e
lazarspa <lazar.spasojevic21@gmail.com>
2023-02-16 10:38:28 +0100

    refactor; changed pets property to the list data type

--- spring-petvet-customers-service/src/main/java/com/lazarspa/customer/service/OwnerServiceImpl.java
+++ spring-petvet-customers-service/src/main/java/com/lazarspa/customer/service/OwnerServiceImpl.java
@@ -5,12 +5,14 @@ import com.lazarspa.customer.dto.OwnerDTO;
 import com.lazarspa.customer.mapper.OwnerMapper;
 import com.lazarspa.customer.model.OwnerDO;
 import com.lazarspa.customer.repository.OwnerRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class OwnerServiceImpl implements OwnerService{
 
     @Autowired
@@ -20,6 +22,9 @@ public class OwnerServiceImpl implements OwnerService{
 
     @Override
     public List<OwnerDTO> findAll() {
+
+        log.info("Fetching all owners");
+
         List<OwnerDO> all = ownerRepository.findAll();
         return ownerMapper.toDtoList(all);
     }
